a375cd5e61d9fa66ccab1da7cd31e0f6ab70a302
Jonathan Balls <j.balls@btopenworld.com>
2017-03-04 16:26:24 +0000

    Added chartjs ready for real data

--- web/static/app.js
+++ web/static/app.js
@@ -79,6 +79,7 @@ var dodata = {
         }],
 };
 
+if (tagctx) {
 var myDoughnutChart = new Chart(tagctx, {
     type: 'doughnut',
     data: dodata,
@@ -89,4 +90,41 @@ var myDoughnutChart = new Chart(tagctx, {
         }
     }
 });
+}
+
+var popctx = document.getElementById("tagTimedPopularity");
+var popdata = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "Usage over time",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+        }
+    ]
+};
+
+if (popctx) {
+var myLineChart = new Chart(popctx, {
+    type: 'line',
+    data: popdata
+});
+}
 
