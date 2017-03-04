2b3f5bedc963981395d6f1168b8dfc47c3456aa9
Jonathan Balls <j.balls@btopenworld.com>
2017-03-04 09:50:58 +0000

    Add most popular tags to web index

--- web/index.js
+++ web/index.js
@@ -1,11 +1,49 @@
 var express = require('express'),
-    pug = require('pug')
-var app = express()
+    pug = require('pug'),
+    mongo = require('mongodb'),
+    assert = require('assert')
+
+var app = express();
+app.locals.pretty = true;
+
+// Mongo setup
+var MongoClient = mongo.MongoClient
+var mongo_url = "mongodb://localhost:27017/deephack"
+
+//MongoClient.connect(mongo_url, function(err, db) {
+  //assert.equal(null, err);
+  //popularTagsPipeline(db, function(results) {
+    //db.close();
+  //});
+//});
+
+// Get list of top 10 tags
+var popularTagsPipeline = function(db, callback) {
+  var collection = db.collection( 'hacks' );
+  collection.aggregate( 
+      [ { "$project": { "_id": 0, "tags": 1 } },
+        { "$unwind" : "$tags" },
+        { "$group"  : { "_id": "$tags", "count": {"$sum":  1} } },
+        { "$sort"   : {"count" : -1 } },
+        { "$limit"  : 10 }
+      ],	  
+	  function(err, results) {
+        assert.equal(err, null);
+        callback(results);
+      }
+  );
+}
 
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.render('index', { title : 'this is my title' })
+    MongoClient.connect(mongo_url, function(err, db) {
+        assert.equal(null, err);
+        popularTagsPipeline(db, function(results) {
+            res.render('index', { title : 'this is my title', popularTags:results })
+            db.close();
+        });
+    });
 })
 
 app.use('/static', express.static('static'))
--- web/static/app.js
+++ web/static/app.js
@@ -17,3 +17,49 @@ var granimInstance = new Granim({
         }
     }
 });
+
+//
+// Charting
+//
+
+
+// Chart.js
+var ctx = document.getElementById("recent-hacks");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: popularTags.map((value) => value._id),
+        datasets: [{
+            label: '# of Votes',
+            data: popularTags.map((value) => value.count),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
