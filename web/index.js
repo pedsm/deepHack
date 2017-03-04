dc7b3a564c3a080605e59bcef52d0b1621da8e8b
Jonathan Balls <j.balls@btopenworld.com>
2017-03-04 07:54:44 +0000

    templates and static

--- web/index.js
+++ web/index.js
@@ -1,9 +1,14 @@
-var express = require('express')
+var express = require('express'),
+    pug = require('pug')
 var app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index', { title : 'this is my title' })
 })
+
+app.use('/static', express.static('static'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
---
+++ web/static/app.js
@@ -0,0 +1,19 @@
+// Deephack javascript
+
+var granimInstance = new Granim({
+    element: '#nav-canvas',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                //['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ],
+            transitionSpeed: 10000
+        }
+    }
+});
